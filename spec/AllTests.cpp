cc783a5be4e0b2ec2c765d5ff79a824cddfb7bbb
Alex Codreanu <alexcodreanu86@gmail.com>
2016-05-27 17:22:37 -0500

    Make spec run report with colors

--- spec/AllTests.cpp
+++ spec/AllTests.cpp
@@ -1,6 +1,10 @@
 #include "CppUTest/CommandLineTestRunner.h"
+#include "stdio.h"
 
 int main(int ac, char** av)
 {
-      return RUN_ALL_TESTS(ac, av);
+  // Add color parameter to test run
+  char color[] = {'-', 'c', '\0'};
+  av[ac] = color;
+  return RUN_ALL_TESTS(2, av);
 }
